589650aa36088a975d13eb69971e0688636d9cb3
Damien Toulouse <dam.toulouse@gmail.com>
2014-11-26 17:32:43 +0100

    légére modif pour éviter une erreur de close connection en cas de sql exeption

--- taupe_models/src/BDD/CreateTable.java
+++ taupe_models/src/BDD/CreateTable.java
@@ -1,31 +1,36 @@
 package BDD;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
-public class CreateTable
-{
-  public static void main( String args[] )
-  {
-    Connection c = null;
-    Statement stmt = null;
-    try {
-      Class.forName("org.sqlite.JDBC");
-      c = DriverManager.getConnection("jdbc:sqlite:model.db");
-      System.out.println("Opened database successfully");
 
-      stmt = c.createStatement();
-      String sql = "CREATE TABLE Modeles " +
-                   "(NAME           TEXT    NOT NULL, " +
-                   " CHEMIN			TEXT	NOT NULL,"+
-                   " TAG1 TEXT NOT NULL,"+
-                   " TAG2 TEXT NOT NULL)";
-      stmt.executeUpdate(sql);
-      stmt.close();
-      c.close();
-    } catch ( Exception e ) {
-      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-      System.exit(0);
-    }
-    System.out.println("Table created successfully");
-  }
+public class CreateTable {
+	public static void main(String args[]) {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:model.db");
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			String sql = "CREATE TABLE Modeles "
+					+ "(NAME           TEXT    NOT NULL, "
+					+ " CHEMIN			TEXT	NOT NULL," + " TAG1 TEXT NOT NULL,"
+					+ " TAG2 TEXT NOT NULL)";
+			stmt.executeUpdate(sql);
+			stmt.close();
+
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+
+		}
+		finally {
+			try {
+				c.close();
+			} catch (Exception e2) {}
+		}
+		System.out.println("Table created successfully");
+	}
 }
--- taupe_models/src/BDD/DropTable.java
+++ taupe_models/src/BDD/DropTable.java
@@ -1,27 +1,33 @@
 package BDD;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
-public class DropTable
-{
-  public static void main( String args[] )
-  {
-    Connection c = null;
-    Statement stmt = null;
-    try {
-      Class.forName("org.sqlite.JDBC");
-      c = DriverManager.getConnection("jdbc:sqlite:model.db");
-      System.out.println("Opened database successfully");
 
-      stmt = c.createStatement();
-      String sql = "DROP TABLE Modeles";
-      stmt.executeUpdate(sql);
-      stmt.close();
-      c.close();
-    } catch ( Exception e ) {
-      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-      System.exit(0);
-    }
-    System.out.println("Table dropped successfully");
-  }
+public class DropTable {
+	public static void main(String args[]) {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:model.db");
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			String sql = "DROP TABLE Modeles";
+			stmt.executeUpdate(sql);
+			stmt.close();
+
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+
+		}
+		finally {
+			try {
+				c.close();
+			} catch (Exception e2) {}
+		}
+		System.out.println("Table dropped successfully");
+	}
 }
