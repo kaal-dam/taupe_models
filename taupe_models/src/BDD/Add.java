f335ae3b1dc9f0f7c0383c185b0911b8b06f9bca
Florent SICCHIO <f.sicchio@gmail.com>
2014-12-29 16:17:43 +0100

    batard

---
+++ taupe_models/src/BDD/Add.java
@@ -0,0 +1,34 @@
+package BDD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+
+import Main.MainClass;
+
+public class Add {
+	public static boolean toTag(String t, String nom){
+		Connection c = null;
+		String query ="";
+		try{
+			
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:model.db");
+			c.setAutoCommit(false);
+			
+			query = "INSERT INTO Tags values ('"+ t +"');";
+			boolean ret1 = c.createStatement().executeUpdate(query) > 0;
+			query = "INSERT INTO Association values ('" + nom + "','"+ t +"');";
+			boolean ret2 = c.createStatement().executeUpdate(query) > 0;
+			c.commit();
+			c.close();
+			
+			return ret1 && ret2;
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{c.close();}catch(Exception e1){}
+		}
+		return false;
+	}
+}
